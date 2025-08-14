7187d6b207b930c362e2d312909bb5f5d775ef9f
FadelSaid <154960863+FadelSaidd@users.noreply.github.com>
2025-08-14 13:30:30 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Fadel Said',
-  role: 'Front End Engineer',
+  role: 'Front Starrt Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
